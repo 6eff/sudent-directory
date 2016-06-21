af61c993f8aa207aeae9bd68504be4f317980ab2
6eff <6eff@mail.ru>
2016-06-21 17:09:01 +0300

    directory refactored

--- directory.rb
+++ directory.rb
@@ -1,18 +1,9 @@
 student_count = 11
+students = ["Dr. Hannibal Lector", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West", "Terminator", "Fredie Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
 puts
 puts "The students of The Villains Academy"
 puts
 puts "____________________________________"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts 'Michael Corleone'
-puts 'Alex DeLarge'
-puts 'The Wicked Witch of the West'
-puts 'Terminator'
-puts 'Fredie Krueger'
-puts 'The Joker'
-puts 'Joffrey Baratheon'
-puts 'Norman Bates'
 puts
-puts "Overall we have #{student_count} great students"
+students.each {|x| puts x}
+puts "Overall we have #{students.count} great students"
