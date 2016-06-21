37a3992b99e9c7557f7711e8524cfa9a1503670c
6eff <6eff@mail.ru>
2016-06-21 17:19:01 +0300

    added methods for each print

--- directory.rb
+++ directory.rb
@@ -1,9 +1,21 @@
-student_count = 11
 students = ["Dr. Hannibal Lector", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West", "Terminator", "Fredie Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
+
+def print_header
+  puts "The students of The Villains Academy"
+  puts "____________________________________"
+  puts
+end
+
+def print_students(names)
+  names.each {|name| puts name}
 puts
-puts "The students of The Villains Academy"
+end
+
+def print_footer(names)
+puts "Overall we have #{names.count} great students"
 puts
-puts "____________________________________"
-puts
-students.each {|x| puts x}
-puts "Overall we have #{students.count} great students"
+end
+
+print_header
+print_students(students)
+print_footer(students)
