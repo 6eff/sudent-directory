40c0739a614ec9a9753a2017e4ad262d02be5617
6eff <6eff@mail.ru>
2016-06-21 16:49:39 +0300

    list of students aded

---
+++ directory.rb
@@ -0,0 +1,16 @@
+puts
+puts "The students of The Villains Academy"
+puts
+puts "____________________________________"
+puts "Dr. Hannibal Lector"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts 'Michael Corleone'
+puts 'Alex DeLarge'
+puts 'The Wicked Witch of the West'
+puts 'Terminator'
+puts 'Fredie Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+puts 'Norman Bates'
+puts
