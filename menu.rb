8987db7f8027111671b426e951e55889cc9a0cdb
6eff <6eff@mail.ru>
2016-06-27 00:09:30 +0300

    load file menu added

--- menu.rb
+++ menu.rb
@@ -5,6 +5,7 @@ def print_menu
   puts "1. Input sudents"
   puts "2. Print students"
   puts "3. Save students to a csv file"
+  puts "4. Load students list from csv file"
   puts "9. Exit" # will add some more
 end
 def input_students
@@ -27,6 +28,8 @@ def process_selection(selection)
   show_students
   when "3"
     print_list_to_file
+  when "4"
+    load_students
   when "9"
     puts "Good buy!"
     exit #program terminates
@@ -40,6 +43,14 @@ def print_list_to_file
     students_data = [student[:name], student[:cohort], student[:hobby], student[:country], student[:height]]
     data_line = students_data.join(",")
     file.puts data_line
+  end
+  file.close
+end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
